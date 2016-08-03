3343e762f7c177f1799b1995c5092d210b708cc0
nishuidegou <emb_zb@sina.cn>
2016-08-03 09:10:39 +0800

    test torch conv

--- spectrogram.lua
+++ spectrogram.lua
@@ -7,8 +7,25 @@ voice = audio.load(fname)
 --spect = audio.spectrogram(voice, 8192, 'hann', 512)
 spect = audio.spectrogram(voice, 8192, 'hann', 512)
 
-image.display(spect)
+--image.display(spect)
 img = image.toDisplayTensor(spect)
-imgNarrow = img:narrow(1,3072,1024)
+imgNarrow = img:narrow(1,4096-256,256)
 --filename = io.read()
 image.save(fname .. '.pgm',imgNarrow)
+
+-- cross-correlation
+--[[
+k = torch.rand(10,10)
+corr = torch.xcorr2(imgNarrow,k)
+conv = torch.conv2(imgNarrow,k)
+--print(c:size())
+image.display(corr)
+image.display(conv)
+--]]
+
+
+k = torch.ones(255,255)
+conv = torch.conv2(imgNarrow,k)
+print(conv)
+image.display(conv)
+
